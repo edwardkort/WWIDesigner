de0b59005b25c1c456c416b8d5ba1632b43f516f
Edward Kort <edward.kort@gmail.com>
2012-07-12 10:08:14 -0600

    Removed optimization classes whose functionality was moved to either the geometry package or test XML files.

--- WWIDesigner/src/main/com/wwidesigner/optimization/MouthpieceInterface.java
+++
@@ -1,6 +0,0 @@
-package com.wwidesigner.optimization;
-
-public interface MouthpieceInterface
-{
-	public int reflectanceMultiplier();
-}
--- WWIDesigner/src/main/com/wwidesigner/optimization/TerminationInterface.java
+++
@@ -1,9 +0,0 @@
-package com.wwidesigner.optimization;
-
-import com.wwidesigner.math.StateVector;
-import com.wwidesigner.util.PhysicalParameters;
-
-interface TerminationInterface
-{
-	public StateVector stateVector(double wave_number, PhysicalParameters params);	
-}

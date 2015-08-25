9a648fe3dd08e96b10af6b058e3d3d61e3ed7e46
edwardkort <edward.kort@gmail.com>
2015-08-25 11:52:45 -0600

    Add loop gain maxima console printout to NoteSpectrumPlot.

--- WWIDesigner/src/main/com/wwidesigner/modelling/PlayingRangeSpectrum.java
+++ WWIDesigner/src/main/com/wwidesigner/modelling/PlayingRangeSpectrum.java
@@ -4,9 +4,12 @@
 package com.wwidesigner.modelling;
 
 import java.awt.Color;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
 
 import javax.swing.JFrame;
@@ -27,15 +30,17 @@ import com.wwidesigner.note.Note;
 import com.wwidesigner.note.Tuning;
 
 /**
- * Class to plot the impedance and playing ranges of an instrument
- * over a range of frequencies with specified fingering.
- * Includes the ratio of X/R, and the estimated loop gain.
+ * Class to plot the impedance and playing ranges of an instrument over a range
+ * of frequencies with specified fingering. Includes the ratio of X/R, and the
+ * estimated loop gain.
  */
 public class PlayingRangeSpectrum
 {
 	protected String mName;
-	protected List<Double> actuals;		// min/max frequency if available, nominal frequency otherwise.
-	protected boolean hasMinMax;		// false if actuals contains min/max, true if actuals contains nominal.
+	protected List<Double> actuals; // min/max frequency if available, nominal
+									// frequency otherwise.
+	protected boolean hasMinMax; // false if actuals contains min/max, true if
+									// actuals contains nominal.
 	/**
 	 * Holds impedance spectrum (created by calcImpedance()).
 	 */
@@ -47,50 +52,90 @@ public class PlayingRangeSpectrum
 	protected Map<Double, Double> mGain;
 
 	/**
+	 * Holds loop gain maxima
+	 */
+	public Map<Double, Double> mGainMaxima;
+
+	private int dataPointIndex;
+	private double prevFreq;
+	private double prevLoopGain;
+	private double prevPrevLoopGain;
+
+	/**
 	 * Add or replace a point in the spectrum.
 	 */
-	protected void setDataPoint(double frequency, Complex impedance, Double loopGain)
+	protected void setDataPoint(double frequency, Complex impedance,
+			Double loopGain)
 	{
 		mImpedance.put(frequency, impedance);
 		mGain.put(frequency, loopGain);
+		findLoopGainMaximum(frequency, loopGain);
 	}
 
-	protected void calcImpedance(InstrumentCalculator calculator, Fingering fingering,
-			double freqStart, double freqEnd, int nfreq)
+	private void findLoopGainMaximum(double frequency, Double loopGain)
+	{
+		if ((dataPointIndex >= 2) && (prevLoopGain > loopGain)
+				&& (prevLoopGain > prevPrevLoopGain))
+		{
+			// We have found a loop gain maximum.
+			mGainMaxima.put(prevFreq, prevLoopGain);
+		}
+
+		dataPointIndex++;
+		prevPrevLoopGain = prevLoopGain;
+		prevLoopGain = loopGain;
+		prevFreq = frequency;
+	}
+
+	protected void printLoopGainMaxima()
+	{
+		System.out.println("\nLoop gain maxima for " + mName);
+		DecimalFormat decFormat = new DecimalFormat("0.00");
+		Set<Entry<Double, Double>> entries = mGainMaxima.entrySet();
+		for (Entry<Double, Double> entry : entries)
+		{
+			System.out.println(decFormat.format(entry.getKey()) + " Hz, gain "
+					+ decFormat.format(entry.getValue()));
+		}
+	}
+
+	protected void calcImpedance(InstrumentCalculator calculator,
+			Fingering fingering, double freqStart, double freqEnd, int nfreq)
 	{
 		Note myNote = fingering.getNote();
 		mName = "Note";
 		String holeString = fingering.toString();
-		if (myNote.getName() != null && ! myNote.getName().isEmpty())
+		if (myNote.getName() != null && !myNote.getName().isEmpty())
 		{
 			mName += " " + myNote.getName();
-			if (holeString != null && ! holeString.isEmpty())
+			if (holeString != null && !holeString.isEmpty())
 			{
 				mName += " (" + holeString + ")";
 			}
 		}
-		else {
-			mName += holeString;				
+		else
+		{
+			mName += holeString;
 		}
 		String instrName = calculator.instrument.getName();
-		if (instrName != null && ! instrName.isEmpty())
+		if (instrName != null && !instrName.isEmpty())
 		{
 			mName += " on " + instrName;
 		}
 		actuals = new ArrayList<Double>();
 		hasMinMax = false;
 
-		if ( myNote.getFrequencyMin() != null )
+		if (myNote.getFrequencyMin() != null)
 		{
 			actuals.add(myNote.getFrequencyMin());
 			hasMinMax = true;
 		}
-		if ( myNote.getFrequencyMax() != null )
+		if (myNote.getFrequencyMax() != null)
 		{
 			actuals.add(myNote.getFrequencyMax());
 			hasMinMax = true;
 		}
-		if ( ! hasMinMax && myNote.getFrequency() != null )
+		if (!hasMinMax && myNote.getFrequency() != null)
 		{
 			actuals.add(myNote.getFrequency());
 		}
@@ -103,7 +148,7 @@ public class PlayingRangeSpectrum
 			double freq = freqStart + i * freqStep;
 			Complex zAc = calculator.calcZ(freq);
 			Double gain = calculator.calcGain(freq, zAc);
-			setDataPoint(freq, zAc, gain );
+			setDataPoint(freq, zAc, gain);
 		}
 	}
 
@@ -178,13 +223,11 @@ public class PlayingRangeSpectrum
 				DefaultChartModel modelActuals;
 				if (hasMinMax)
 				{
-					modelActuals = new DefaultChartModel(
-						"Actual Frequency");
+					modelActuals = new DefaultChartModel("Actual Frequency");
 				}
 				else
 				{
-					modelActuals = new DefaultChartModel(
-							"Target Frequency");
+					modelActuals = new DefaultChartModel("Target Frequency");
 				}
 				for (Map.Entry<Double, Complex> point : mImpedance.entrySet())
 				{
@@ -197,7 +240,7 @@ public class PlayingRangeSpectrum
 				{
 					double x = point.getKey();
 					double g = point.getValue();
-					if ( g >= 1.0 )
+					if (g >= 1.0)
 					{
 						modelGain.addPoint(x, g);
 					}
@@ -206,9 +249,9 @@ public class PlayingRangeSpectrum
 						modelGainLow.addPoint(x, g);
 					}
 				}
-				for ( Double freq : actuals )
+				for (Double freq : actuals)
 				{
-					modelActuals.addPoint(freq,-0.4);
+					modelActuals.addPoint(freq, -0.4);
 				}
 
 				Chart chart = new Chart();
@@ -220,14 +263,17 @@ public class PlayingRangeSpectrum
 				gainAxis.setPlacement(AxisPlacement.TRAILING);
 				chart.addYAxis(gainAxis);
 				ChartStyle styleRatio = new ChartStyle(Color.black, false, true);
-				ChartStyle styleGain  = new ChartStyle(Color.green, PointShape.CIRCLE);
-				ChartStyle styleGainLow  = new ChartStyle(Color.red, PointShape.CIRCLE);
+				ChartStyle styleGain = new ChartStyle(Color.green,
+						PointShape.CIRCLE);
+				ChartStyle styleGainLow = new ChartStyle(Color.red,
+						PointShape.CIRCLE);
 				chart.addModel(modelRatio, impedanceAxis, styleRatio);
 				chart.addModel(modelGain, gainAxis, styleGain);
 				chart.addModel(modelGainLow, gainAxis, styleGainLow);
-				if ( actuals.size() > 0 )
+				if (actuals.size() > 0)
 				{
-					ChartStyle styleActuals  = new ChartStyle(Color.gray, PointShape.DIAMOND, Color.gray);
+					ChartStyle styleActuals = new ChartStyle(Color.gray,
+							PointShape.DIAMOND, Color.gray);
 					styleActuals.setPointSize(8);
 					chart.addModel(modelActuals, impedanceAxis, styleActuals);
 				}
@@ -243,8 +289,9 @@ public class PlayingRangeSpectrum
 	}
 
 	/**
-	 * Plot the impedance and playing ranges for a given calculator
-	 * and fingering, using default plot parameters.
+	 * Plot the impedance and playing ranges for a given calculator and
+	 * fingering, using default plot parameters.
+	 * 
 	 * @param calculator
 	 * @param fingering
 	 */
@@ -254,29 +301,35 @@ public class PlayingRangeSpectrum
 	}
 
 	/**
-	 * Plot the impedance and playing ranges for a given calculator and fingering.
+	 * Plot the impedance and playing ranges for a given calculator and
+	 * fingering.
+	 * 
 	 * @param calculator
 	 * @param fingering
-	 * @param freqRangeBelow - Range of frequencies to plot below fingered note,
-	 * 			as a fraction < 1 of the note.
-	 * @param freqRangeAbove - Range of frequencies to plot above fingered note,
-	 * 			as a multiple > 1 of the note.
-	 * @param numberPoints - number of points to calculate for plotting.
+	 * @param freqRangeBelow
+	 *            - Range of frequencies to plot below fingered note, as a
+	 *            fraction < 1 of the note.
+	 * @param freqRangeAbove
+	 *            - Range of frequencies to plot above fingered note, as a
+	 *            multiple > 1 of the note.
+	 * @param numberPoints
+	 *            - number of points to calculate for plotting.
 	 */
 	public void plot(InstrumentCalculator calculator, Fingering fingering,
 			double freqRangeBelow, double freqRangeAbove, int numberPoints,
 			final boolean exitOnClose)
 	{
 		double targetFreq;
-		if ( fingering.getNote().getFrequency() != null )
+		if (fingering.getNote().getFrequency() != null)
 		{
 			targetFreq = fingering.getNote().getFrequency();
 		}
-		else if ( fingering.getNote().getFrequencyMax() != null )
+		else if (fingering.getNote().getFrequencyMax() != null)
 		{
 			targetFreq = fingering.getNote().getFrequencyMax();
 		}
-		else {
+		else
+		{
 			targetFreq = 1000.0;
 		}
 		double freqStart = targetFreq * 0.5;
@@ -289,24 +342,33 @@ public class PlayingRangeSpectrum
 		{
 			freqEnd = targetFreq * freqRangeAbove;
 		}
-		calcImpedance(calculator, fingering, freqStart, freqEnd,
-				numberPoints);
+
+		// Set up loop gain maxima find
+		mGainMaxima = new TreeMap<Double, Double>();
+		dataPointIndex = 0;
+		prevFreq = 0.;
+		prevLoopGain = 0.;
+		prevPrevLoopGain = 0.;
+
+		calcImpedance(calculator, fingering, freqStart, freqEnd, numberPoints);
 		// plotImpedanceSpectrum();
+		printLoopGainMaxima();
 		plotPlayingRange(exitOnClose);
 
 	}
-	
+
 	/**
-	 * Plot the impedance and playing range for each note in a given list of notes,
-	 * for a given calculator and fingering.
+	 * Plot the impedance and playing range for each note in a given list of
+	 * notes, for a given calculator and fingering.
+	 * 
 	 * @param calculator
 	 * @param tuning
 	 * @param noteList
 	 */
-	public static void plotNotes( InstrumentCalculator calculator, Tuning tuning,
-			int[] noteList )
+	public static void plotNotes(InstrumentCalculator calculator,
+			Tuning tuning, int[] noteList)
 	{
-		for ( int noteIndex : noteList )
+		for (int noteIndex : noteList)
 		{
 			PlayingRangeSpectrum spectrum = new PlayingRangeSpectrum();
 			spectrum.plot(calculator, tuning.getFingering().get(noteIndex));
